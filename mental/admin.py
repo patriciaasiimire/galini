360c997c44330027e467902d84d7c9389d7c710e
asiimire <asiimirepatricia26@gmail.com>
2024-07-22 13:27:49 +0300

    registered models to admin.py

--- mental/admin.py
+++ mental/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import TherapistProfile, Review
 
-admin.sites.register()
-admin.sites.register()
-admin.sites.register()
+admin.sites.register(TherapistProfile)
+admin.sites.register(Review)
