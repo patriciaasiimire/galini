85e3b822aea76fef38b3f8d59451e906f5ae0d6f
asiimire <asiimirepatricia26@gmail.com>
2024-07-22 21:27:43 +0300

    therapist profile

--- mental/urls.py
+++ mental/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     # Mental Health Home Page
     path('', views.home, name='home'),
-    path('therapist_profile/', views.therapist_profile, name='therapist_profile'),
+    path('therapist_profile', views.therapist_profile, name='therapist_profile'),
+    # path('therapist/<int:pk>', views.therapist, name='therapist'),
+    path('therapist', views.therapist, name='therapist'),
 ]
 
--- mental/views.py
+++ mental/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from .models import TherapistProfile
 
 def home(request):
     return render(request, 'home.html')
 
 def therapist_profile(request):
-    return render(request, 'therapist_profile.html')
+    return render(request, 'therapist_profile.html', {})
+
+def therapist(request):
+    # therapist = TherapistProfile.objects.all(id=pk)
+    return render(request, 'therapist.html', {'therapist': therapist})
